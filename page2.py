1e6cf0d3ae6105161d9bbddee2ea2147d21bf0a9
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-18 18:40:45 -0400

    Update page2.py

--- page2.py
+++ page2.py
@@ -1,5 +1,22 @@
 import streamlit as st
 import pandas as pd
-
+from sqlalchemy import create_engine
+from snowflake.sqlalchemy import URL
 
 st.write ("Welcome")
+
+
+engine = create_engine(URL(
+    account = 'mgvioku-jw32137',
+    user = 'alekyakastury',
+    password = '@Noon1240',
+    database = 'CUSTOMER',
+    schema = 'public',
+    warehouse = 'COMPUTE_WH',
+    role='ACCOUNTADMIN',
+))
+connection = engine.connect()
+try:
+    st.write(connection.execute('SELECT * FROM CUSTOMER LIMIT 1'))
+finally:
+    connection.close()
