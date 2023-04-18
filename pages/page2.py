345a66344298f941e6aa79756abb72dfbbc7633e
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-18 19:59:05 -0400

    Create page2.py

---
+++ pages/page2.py
@@ -0,0 +1,31 @@
+import streamlit as st
+import plotly.graph_objs as go
+import pandas as pd
+import numpy as np
+import datetime as dt
+import snowflake.connector as sf
+from sqlalchemy import create_engine
+from snowflake.sqlalchemy import URL
+import matplotlib.pyplot as plt
+
+#trying chemy
+engine = create_engine(URL(
+    account = 'dl84836.us-east-2.aws',
+    user = 'alekyakastury',
+    password = '@Noon1240',
+    database = 'CUSTOMER',
+    schema = 'PUBLIC',
+    warehouse = 'compute_wh'
+))
+
+st.set_page_config(
+    page_title="Hello",
+    page_icon="👋",
+)
+
+st.sidebar.success("Select a demo above.")
+
+st.write ("Welcome")
+query="""SELECT * FROM CUSTOMER LIMIT 1;"""
+df=pd.read_sql_query(query,engine)
+st.write(df)
