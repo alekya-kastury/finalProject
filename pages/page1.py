05aba9a1aa1ad367a6281e62e4e92685f43e6cf0
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-18 23:52:45 -0400

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -1,14 +1,9 @@
 import streamlit as st
 from st_clickable_images import clickable_images
 
-clicked = clickable_images(
-    ["https://icons.iconarchive.com/icons/custom-icon-design/pretty-office-7/256/Save-icon.png"],
-    div_style={"display": "flex", "justify-content": "left", "flex-wrap": "wrap"},
-    img_style={"margin": "10px", "height": "250px"},
-    key="clicked_images",
+st.markdown('''
+    <a href="https://docs.streamlit.io">
+        <img src="https://media.tenor.com/images/ac3316998c5a2958f0ee8dfe577d5281/tenor.gif" />
+    </a>''',
+    unsafe_allow_html=True
 )
-
-if clicked == 0:
-    st.subheader("Saving Report..")
-else:
-    st.subheader(f"Selected Image No: #{clicked}")
