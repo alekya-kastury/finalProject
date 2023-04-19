4d6a8c01b123db04eee20472e45b2dcc72e2fa1e
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-19 17:51:15 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -99,7 +99,7 @@ df_no_of_cust=pd.read_sql_query(query,engine)
 no_of_customers=df_no_of_cust['no_of_customers'][0]
 
 if year==1998 and month==1:
-    percentage=100
+    percentage_cust=100
 elif month==1:
     prev_year=year-1
     prev_month=12
@@ -123,8 +123,29 @@ no_of_customers_prev=df_no_of_cust_prev['no_of_customers'][0]
 percentage_cust=((no_of_customers-no_of_customers_prev)/no_of_customers_prev)*100
 
 ###########################################################################################
+##############################BLOCK 3####################################################
+if year==1998 and month==1:
+    percentage_return_cust=0
+elif month==1:
+    prev_year=year-1
+    prev_month=12
+    query="""SELECT
+    (COUNT(DISTINCT SS_CUSTOMER_SK) - COUNT(DISTINCT CASE WHEN DD.D_YEAR < {} AND DD.D_MOY<{} THEN SS_CUSTOMER_SK END)) * 100.0
+    / COUNT(DISTINCT SS_CUSTOMER_SK) as returning_customers_percentage
+    FROM STORE_SALES SS INNER JOIN DATE_DIM DD ON SS.SS_SOLD_DATE_SK=DD.D_DATE_SK
+    WHERE DD.D_YEAR = {} AND DD.D_MOY={};""".format(year,month,year,month)
+else:
+    prev_month=month-1
+    query="""SELECT
+    (COUNT(DISTINCT SS_CUSTOMER_SK) - COUNT(DISTINCT CASE WHEN DD.D_YEAR = {} AND DD.D_MOY<{} THEN SS_CUSTOMER_SK END)) * 100.0
+    / COUNT(DISTINCT SS_CUSTOMER_SK) as returning_customers_percentage
+    FROM STORE_SALES SS INNER JOIN DATE_DIM DD ON SS.SS_SOLD_DATE_SK=DD.D_DATE_SK
+    WHERE DD.D_YEAR = {} AND DD.D_MOY={};""".format(year,month,year,month)
+    
+df_ret_cust=pd.read_sql_query(query,engine)
+ret_customers=df_ret_cust['returning_customers_percentage'][0]    
 
-
+#########################################################################################
 # Create a container for the metrics
 with st.beta_container():
     # Create two columns for the metrics
@@ -134,6 +155,6 @@ with st.beta_container():
     with col2:
         st.metric('Number of Customers', shorten_num(no_of_customers),delta=str(round(percentage_cust,1))+'%')
     with col3:
-        st.metric('Repeat Purchase Rate', '300')
+        st.metric('Returning Customers', str(shorten_num(ret_customers))+'%')
     with col4:
         st.metric('Average Order Value', '300')
