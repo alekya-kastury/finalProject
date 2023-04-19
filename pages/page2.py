2dd3563a3b8a2fde48f9bf65d251a648d009aa1a
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-19 14:44:41 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -55,5 +55,28 @@ df=pd.read_sql_query(query,engine)
 
 val=df['sales']
 
+def shortened_num(number):    
+    if number >= 1000000000:
+        shortened_num = str(round(number/1000000000, 1)) + "B"
+    elif number >= 1000000:
+        shortened_num = str(round(number/1000000, 1)) + "M"
+    elif number >= 1000:
+        shortened_num = str(round(number/1000, 1)) + "K"
+    else:
+        shortened_num = str(number)
+    return shortened_num
+
+# Create a container for the metrics
+with st.beta_container():
+    # Create two columns for the metrics
+    col1, col2, col3, col4 = st.beta_columns(4)
+    with col1:
+        st.metric(label="Revenue", value=shorten_num(val))
+    with col2:
+        st.metric('New Customers', '200')
+    with col3:
+        st.metric('Repeat Purchase Rate', '300')
+    with col4:
+        st.metric('Average Order Value', '300')
 
 
