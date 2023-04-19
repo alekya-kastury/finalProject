c1332cead698507391b8ce7ec485a4b94e587bd5
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-18 23:17:34 -0400

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -3,20 +3,29 @@ import streamlit as st
 def page1():
     st.title('Page 1')
     st.write('This is the first page of my app.')
-
+    
+    # Create a link to page 2
+    if st.button('Go to Page 2'):
+        st.experimental_rerun()
+        st.experimental_show('Page 2')
+    
 def page2():
     st.title('Page 2')
     st.write('This is the second page of my app.')
+    
+    # Create a link to page 1
+    if st.button('Go to Page 1'):
+        st.experimental_rerun()
+        st.experimental_show('Page 1')
 
-# Define a dictionary that maps the user's selection in the sidebar to the corresponding page function
+# Define a dictionary that maps the page names to the corresponding functions
 pages = {
-    "Page 1": page1,
-    "Page 2": page2
+    'Page 1': page1,
+    'Page 2': page2
 }
 
-# Create a sidebar menu with radio buttons
-selection = st.sidebar.selectbox("Go to", list(pages.keys()))
+# Create a selectbox for page navigation
+selection = st.sidebar.selectbox('Go to', list(pages.keys()))
 
 # Call the appropriate page function based on the user's selection
 pages[selection]()
-
