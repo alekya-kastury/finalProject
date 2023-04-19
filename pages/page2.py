270b1a0aeb8371a7f69af0a52aef5218615b6c54
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-19 15:31:02 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -88,7 +88,7 @@ with st.beta_container():
     # Create two columns for the metrics
     col1, col2, col3, col4 = st.beta_columns(4)
     with col1:
-        st.metric(label="Revenue", value=shorten_num(revenue_current),delta=percentage)
+        st.metric(label="Revenue", value=shorten_num(revenue_current),delta=rount(percentage,1))
     with col2:
         st.metric('New Customers', '200')
     with col3:
