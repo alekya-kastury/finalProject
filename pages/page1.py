12f353004a9ad5cd54a4c2eeba881ceb2cb9f33a
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-18 23:48:27 -0400

    Update page1.py

--- pages/page1.py
+++ pages/page1.py
@@ -4,7 +4,7 @@ from st_clickable_images import clickable_images
 clicked = clickable_images(
     [
         "https://images.unsplash.com/photo-1565130838609-c3a86655db61?w=700",
-        "https://images.unsplash.com/photo-156537219z5458-9de0b320ef04?w=700",
+        "https://images.unsplash.com/photo-1565372195458-9de0b320ef04?w=700",
         "https://images.unsplash.com/photo-1582550945154-66ea8fff25e1?w=700",
         "https://images.unsplash.com/photo-1591797442444-039f23ddcc14?w=700",
         "https://images.unsplash.com/photo-1518727818782-ed5341dbd476?w=700",
@@ -14,6 +14,4 @@ clicked = clickable_images(
     img_style={"margin": "5px", "height": "200px"},
 )
 
-
 st.markdown(f"Image #{clicked} clicked" if clicked > -1 else "No image clicked")
-
