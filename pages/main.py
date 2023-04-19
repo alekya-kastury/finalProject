a0913a1177e6b1b086d2c192df693d18827dcfea
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-18 23:24:14 -0400

    Update main.py

--- pages/main.py
+++ pages/main.py
@@ -4,19 +4,15 @@ def page1():
     st.title('Page 1')
     st.write('This is the first page of my app.')
     
-    # Create a link to page 2
-    if st.button('Go to Page 2'):
-        st.experimental_rerun()
-        st.experimental_show('Page 2')
-    
+    # Create a clickable image that navigates to page 2
+    st.write('Click on the image to go to Page 2')
+    image = st.image('https://www.pixelstalk.net/wp-content/uploads/images6/Aesthetic-Cloud-Backgrounds-Free-Download.jpg', use_column_width=True)
+    url = 'https://www.pixelstalk.net/wp-content/uploads/images6/Aesthetic-Cloud-Backgrounds-Free-Download.jpg'
+    image.markdown(f'<a href="{url}">{image._repr_html_()}</a>', unsafe_allow_html=True)
+
 def page2():
     st.title('Page 2')
     st.write('This is the second page of my app.')
-    
-    # Create a link to page 1
-    if st.button('Go to Page 1'):
-        st.experimental_rerun()
-        st.experimental_show('Page 1')
 
 # Define a dictionary that maps the page names to the corresponding functions
 pages = {
@@ -24,4 +20,8 @@ pages = {
     'Page 2': page2
 }
 
-list(pages.keys())
+# Create a selectbox for page navigation
+selection = st.sidebar.selectbox('Go to', list(pages.keys()))
+
+# Call the appropriate page function based on the user's selection
+pages[selection]()
