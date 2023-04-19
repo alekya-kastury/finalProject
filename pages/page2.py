e56dc56394b9768eaae9b08b27eddcb6a2a86011
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-19 15:10:17 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -24,16 +24,8 @@ st.set_page_config(
     page_title="Customer Analysis Dashboard",
 )
 
-# get the distinct year from the database
-#distinct_year_query = """SELECT DISTINCT DD.D_YEAR FROM DATE_DIM DD WHERE DD.D_YEAR IN (1998,1999,2000,2001,2002);"""
-#distinct_year = pd.read_sql_query(distinct_year_query, engine)['d_year'].tolist()
-
 # create a dropdown for the year parameter with the distinct state values
 year = st.sidebar.selectbox('Year', [1998,1999,2000,2001,2002])
-
-# get the distinct year from the database
-#distinct_month_query = """SELECT DISTINCT DD.D_MOY FROM DATE_DIM DD;"""
-#distinct_month = pd.read_sql_query(distinct_month_query, engine)['d_moy'].tolist()
 
 # create a dropdown for the year parameter with the distinct state values
 month = st.sidebar.selectbox('Month', [1,2,3,4,5,6,7,8,9,10,11,12])
@@ -50,10 +42,24 @@ DD.D_MOY={}
 group by 
 DD.D_YEAR, DD.D_MOY""".format(year,month)
 
-df=pd.read_sql_query(query,engine)
-#st.write (df)
+df_rev_current=pd.read_sql_query(query,engine)
+revenue_current=df_rev_current['sales'][0]
 
-val=df['sales'][0]
+query="""SELECT SUM(SS_NET_PAID) as sales
+FROM 
+STORE_SALES SS INNER JOIN DATE_DIM DD
+ON
+SS.SS_SOLD_DATE_SK=DD.D_DATE_SK
+WHERE 
+DD.D_YEAR={} and
+DD.D_MOY={}-1
+group by 
+DD.D_YEAR, DD.D_MOY""".format(year,month)
+
+df_rev_prev=pd.read_sql_query(query,engine)
+revenue_prev=df_rev_prev['sales'][0]
+
+percentage=((revenue_current-revenue_prev)/revenue_prev)*100
 
 def shorten_num(number):    
     if number >= 1000000000:
@@ -71,7 +77,7 @@ with st.beta_container():
     # Create two columns for the metrics
     col1, col2, col3, col4 = st.beta_columns(4)
     with col1:
-        st.metric(label="Revenue", value=shorten_num(val),delta="This is a subtext")
+        st.metric(label="Revenue", value=shorten_num(revenue_current),delta=percentage)
     with col2:
         st.metric('New Customers', '200')
     with col3:
