193688de0bf872a7c632a09996b1afb424f7ac72
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-19 23:14:29 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -202,7 +202,7 @@ group by DD.D_YEAR;""".format(month)
 
 def run_query_plot(query):
     df=pd.read_sql_query(query,engine)
-    c = alt.Chart(df).mark_line().encode(x='year', y='count_of_customers')
+    c = alt.Chart(df,title='Count of customers per year').mark_line().encode(x='year', y='count_of_customers')
     st.altair_chart(c)
  
 run_query_plot(query)
