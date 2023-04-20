515516daee98a549929e8c99453b7a58e35b7530
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-19 21:37:27 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -200,7 +200,7 @@ WHERE DD.D_MOY={}
 group by DD.D_YEAR, DD.D_MOY LIMIT 2;""".format(month)
 
 df=pd.read_sql_query(query,engine)
-st.write(df['YEAR'])
+st.write(df)
 
 @st.cache
 def plot_graph(query,x_v,y_v):
