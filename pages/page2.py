f7f74dc5aedac51bb048e794a6ca88bf60e2f0bc
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-19 22:53:47 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -199,11 +199,10 @@ ON SS.SS_SOLD_DATE_SK=DD.D_DATE_SK
 WHERE DD.D_MOY={}
 group by DD.D_YEAR;""".format(month)
 
-@st.cache_data
-def run_query_plot(query):
-    df=pd.read_sql_query(query,engine)
-    c = alt.Chart(df).mark_line().encode(x='year', y='count_of_customers')
-    st.altair_chart(c)
+#def run_query_plot(query):
+df=pd.read_sql_query(query,engine)
+c = alt.Chart(df).mark_line().encode(x='year', y='count_of_customers')
+st.altair_chart(c)
     
-run_query_plot(query)
+#run_query_plot(query)
  
