a6c4c3d06624c15d62f43fbfc32b821593d41a92
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-19 23:26:20 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -200,6 +200,7 @@ ON SS.SS_SOLD_DATE_SK=DD.D_DATE_SK
 WHERE DD.D_MOY={}
 group by DD.D_YEAR;""".format(month)
 
+@st.cache_data
 def run_query_plot(query):
     df=pd.read_sql_query(query,engine)
     c = alt.Chart(df,title='Count of customers per year').mark_line().encode(x='year', y='count_of_customers')
