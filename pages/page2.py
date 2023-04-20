c08ebcc0fa879fbe866e02d1a26e40f9c95c1475
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-19 22:38:13 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -199,7 +199,7 @@ ON SS.SS_SOLD_DATE_SK=DD.D_DATE_SK
 WHERE DD.D_MOY={}
 group by DD.D_YEAR;""".format(month)
 
-@st.cache
+@st.cache_data
 def run_query_plot(query):
     df=pd.read_sql_query(query,engine)
     st.line_chart(df)
