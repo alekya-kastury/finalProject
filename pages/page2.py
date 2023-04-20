01b2d6d4727cc7aaba79514598d7906f05bed271
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-20 01:40:11 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -229,8 +229,8 @@ run_query_plot(query)
 st.sidebar.title ('Revenue per Demographic') 
 
 query="""SELECT sum(SS_NET_PAID) as REVENUE_MEN FROM STORE_SALES SS INNER JOIN DATE_DIM DD ON SS_SOLD_DATE_SK=D_DATE_SK
-WHERE SS_CUSTOMER_SK IN 
-(SELECT DISTINCT CD_DEMO_SK FROM CUSTOMER_DEMOGRAPHICS WHERE CD_GENDER='M');"""
+WHERE WHERE DD.D_YEAR={} AND DD.D_MOY={} SS_CUSTOMER_SK IN 
+(SELECT DISTINCT CD_DEMO_SK FROM CUSTOMER_DEMOGRAPHICS WHERE CD_GENDER='M');""".format(year,month)
 
 total_revenue_men=run_query(query,'revenue_men')
 st.sidebar.markdown('MEN')                     
@@ -238,13 +238,12 @@ st.sidebar.markdown('$'+str(shorten_num(total_revenue_men)))
 
 ###########################################################################################################################
 query="""SELECT sum(SS_NET_PAID) as REVENUE_MEN FROM STORE_SALES SS INNER JOIN DATE_DIM DD ON SS_SOLD_DATE_SK=D_DATE_SK
-WHERE SS_CUSTOMER_SK IN 
-(SELECT DISTINCT CD_DEMO_SK FROM CUSTOMER_DEMOGRAPHICS WHERE CD_GENDER='F');"""
+WHERE WHERE DD.D_YEAR={} AND DD.D_MOY={} SS_CUSTOMER_SK IN 
+(SELECT DISTINCT CD_DEMO_SK FROM CUSTOMER_DEMOGRAPHICS WHERE CD_GENDER='F');""".format(year,month)
 
 total_revenue_women=run_query(query,'revenue_women')
 st.sidebar.markdown('WOMEN')                     
 st.sidebar.markdown('$'+str(shorten_num(total_revenue_women)))  
 
 ###########################################################################################################################
-
 
