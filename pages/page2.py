2c9e8811b2311193da9911b19c2bfb0720d188c5
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-27 19:32:12 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -224,23 +224,9 @@ c2 = c2.properties(width=400, height=400)
  
 
 #################################################################################
-st.markdown(
-    """
-    <style>
-    .shifted {
-        margin-left: 100px;
-    }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
 st.altair_chart(c2)
-
-st.write('<div class="shifted">', unsafe_allow_html=True)
 st.altair_chart(c1)
-st.write('</div>', unsafe_allow_html=True)
-    
+
     
 ##########################################################################################################################
 st.sidebar.title ('Revenue per Demographic') 
