fae65675d03548ce7fe78b40f8f398ad04b99b28
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-27 18:40:24 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -223,7 +223,7 @@ c2 = c2.properties(width=400, height=400)
 #################################################################################
 col1, col2, col3 = st.beta_columns(3)
 with col1:
-    st.altair_chart(c1)
+    st.altair_chart(c2)
     st.markdown("---",unsafe_allow_html=True)  # add vertical spacing with markdown
 
     # apply CSS styles to adjust padding and margin
@@ -238,7 +238,7 @@ with col1:
     </style>
     """,unsafe_allow_html=True)
 with col2:
-    st.altair_chart(c2)
+    st.altair_chart(c)
     st.markdown("---")  # add vertical spacing with markdown
 
     # apply CSS styles to adjust padding and margin
