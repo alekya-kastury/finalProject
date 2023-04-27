0394380d56931c2bb99da2b4b700add5a214a09d
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-27 18:41:47 -0400

    Update page2.py

--- pages/page2.py
+++ pages/page2.py
@@ -232,8 +232,8 @@ with col1:
     .st-cc {
         padding-top: 16px;
         padding-bottom: 16px;
-        margin-left: 10px;
-        margin-right: 10px;
+        margin-left: 20px;
+        margin-right: 20px;
     }
     </style>
     """,unsafe_allow_html=True)
@@ -247,8 +247,8 @@ with col2:
     .st-cc {
         padding-top: 16px;
         padding-bottom: 16px;
-        margin-left: 8px;
-        margin-right: 4px;
+        margin-left: 20px;
+        margin-right: 20px;
     }
     </style>
     """,unsafe_allow_html=True)
