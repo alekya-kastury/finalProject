56769a8887af075b778b4782314b6d73ca11e2b4
alekya-kastury <91427122+alekya-kastury@users.noreply.github.com>
2023-04-28 06:59:57 -0400

    Update page3.py

--- pages/page3.py
+++ pages/page3.py
@@ -9,7 +9,10 @@ from snowflake.sqlalchemy import URL
 import matplotlib.pyplot as plt
 import humanize
 import altair as alt
-
+import pandas as pd
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 st.set_page_config(page_title="Customer Churn Forecast", page_icon=":bar_chart:", layout="wide")
 
@@ -35,3 +38,16 @@ df=pd.read_sql_query(query,engine)
 c1 = alt.Chart(df,title='Active customers').mark_bar().encode(x='customer_status', y='count_of_customers')
 c1 = c1.properties(width=800, height=400)
 st.altair_chart(c1)
+######################################################################################################
+query="""SELECT * FROM CUSTOMER_DEMO_VIEW;"""
+
+df_customer_demo=pd.read_sql_query(query,engine)
+
+query="""SELECT * FROM CUSTOMER_INCOME;"""
+
+df_customer_income=pd.read_sql_query(query,engine)
+
+query="""SELECT * FROM INCOMER_VIEW;"""
+
+df_income_view=pd.read_sql_query(query,engine)
+
